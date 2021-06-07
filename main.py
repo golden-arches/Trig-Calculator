5d0514dd53462bd390fb59ca3a6d446b227d4a8c
golden-arches <69598810+golden-arches@users.noreply.github.com>
2021-06-07 16:43:46 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+from math import *
+
+
+def evaluate(event):
+    res.configure(text="Result: " + str(eval(entry.get())))
+
+def distance(x1,x2,y1,y2):
+    return sqrt(((x2-x1)**2) + ((y2-y1)**2))
+
+def circumference(radius):
+    return 3.14 * 2 * radius
+
+def cir(radius):
+    return 3.14 * 2 * radius
+
+def areaSphere(radius):
+    return 4 * 3.14 * (radius**2)
+
+def volumeSphere(radius):
+    return 4/3*3.14*(radius**3)
+
+def areaSquare(w,h):
+    return w * h
+
+def midpoint(x1,x2,y1,y2):
+    return (str(((x2+x1)/2)) + ", " + str(((y2+y1)/2)))
+
+functions = ["distance(x1,x2,y1,y2)",
+             "circumference(radius) (or cir(radius))",
+             "areaSphere(radius)",
+             "volumeSphere(radius)",
+             "areaSquare(width, height)",
+             "midpoint(x1,x2,y1,y1)"]
+
+window = tk.Tk()
+
+window.geometry("640x860")
+tk.Label(window, text="Your Expression:",font=('Helvetica', 18, 'bold')).pack()
+entry = tk.Entry(window,width=20)
+entry.bind("<Return>", evaluate)
+entry.pack()
+res = tk.Label(window,font=('Helvetica', 11, 'bold'))
+res.pack()
+tk.Label(text="\n").pack()
+for b in functions:
+    tk.Label(text=b + "\n").pack()
+window.mainloop()
